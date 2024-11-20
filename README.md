4330b0cb477f93fa25a8771e78c77700c77e5bd6
RaddishAI <petter.arbeid@gmail.com>
2024-11-20 21:47:40 +0100

    test deployment

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # css2.0
+
+npm run dev - development mode
+npm run build - publishing mode
+husk å bruk npm run build for å bygg. 
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,html,js}"],
+  content: [
+    "./*.html", // Includes root-level HTML files like index.html
+    "./profile/**/*.html", // Includes all HTML files in the profile directory
+    "./feed/**/*.html", // Includes all HTML files in the feed directory
+    "./src/**/*.{js,ts,jsx,tsx}", // Includes any JavaScript/TypeScript files in src
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
